9a61fbe65443f5d375e212364f2aff30fc6fea35
Igor Varga <ivarhauzh@gmail.com>
2020-10-06 22:11:08 +0200

    Daily commit

--- gc_model.py
+++ gc_model.py
@@ -48,13 +48,43 @@ def distance_metrics(mask):
 
 
 
-def gc_method(img, narrow_mask, init_mask, k):
+def gc_method(img,
+              narrow_mask,
+              init_mask,
+              k):
 
     # ===============PREPROC_ get
+    # get overlap window to not process the all dataset (maybe)
+    print(1)
+    x0, x1, y0, y1, z1, z2 = (np.argmax(init_mask, 0)).min(), (np.argmax(init_mask, 0)).max(), \
+                             (np.argmax(init_mask, 1)).min(), (np.argmax(init_mask, 1)).max(), \
+                             (np.argmax(init_mask, 2)).min(), (np.argmax(init_mask, 2)).max(), \
+
+    #GET DISTANCE METRICS
+
     dist_marker = distance_metrics(narrow_mask)
 
+    # Calculate FOREGROUND AND BACKGROUND SEED
 
+    Fw, Bw = np.zeros(shape=img.shape), np.zeros(shape=img.shape)
     graph = maxflow.Graph[float](img.shape[1]^3, img.shape[1]^3)
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            for k in range(img.shape[2]):
+                if not narrow_mask[i, j, k]:
+                    Bw[i,j,k] = 1
+                if init_mask[i, j, k]:
+                    Fw[i,j,k] = 1
+                pass
+
+    # calculate mean in narrow mask (in paper it's WM)
+    WM_mean = (np.ma.masked_array(img,mask=not narrow_mask)).mean()
+    # Calculate outside mean value (paper threshold)
+    thr = (np.ma.masked_array(img, mask=init_mask)).mean()
+
+    # --------------------- Calculate edges ----------------------
+
+
 
     length,width,height = img.shape[0],img.shape[1],img.shape[2]
     grid = graph.add_grid_nodes(img.shape)
@@ -62,19 +92,12 @@ def gc_method(img, narrow_mask, init_mask, k):
 
 
 
-    F,B = np.zeros(shape=img.shape), np.zeros(shape=img.shape)
+
     # add grid
     graph.add_grid_tedges(grid,img, img.max() - img)
     res_MF = graph.maxflow()
     I_o = graph.get_grid_segments(grid)
-    for i in range(img.shape[0]):
-        for j in range(img.shape[1]):
-            for k in range(img.shape[2]):
-                if not narrow_mask[i, j, k]:
-                    B[i,j,k] = 1
-                if init_mask[i, j, k]:
-                    F[i,j,k] = 1
-                pass
+
 
 
     pass
